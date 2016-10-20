8877f12a5fce925e364625668d9436786e7834d9
neekburm <johnsonscottd@gmail.com>
2016-10-20 07:58:49 -0500

    Ch. 5 - Processing a POST Request on the Server

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,4 +1,5 @@
 import os
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
@@ -10,7 +11,7 @@ driver = webdriver.Chrome(chromedriver)
 driver.get("http://stackoverflow.com")
 driver.quit()
 
-class NewVisitorTest(unittest.TestCase):  
+class NewVisitorTest(LiveServerTestCase):  
 
     def setUp(self):  
         self.browser = webdriver.Chrome(chromedriver)
@@ -22,19 +23,19 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):  
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
 
         # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
-        header_text = self.browser.find_element_by_tag_name('h1').header_text
+        header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
 
         # She is invited to enter a to-do item straight away
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(
-        	inputbox.get_attribute('placeholder'),
-        	'Enter a to-do-item'
-        )	
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )   
 
         # She types "Buy peacock feathers" into a text box (Edith's hobby
         # is tying fly-fishing lures)
@@ -43,11 +44,11 @@ class NewVisitorTest(unittest.TestCase):
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list table
         inputbox.send_keys(Keys.ENTER)
-
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
-        	any(row.text == '1: Buy peacock feathers' for row in rows)
+            any(row.text == '1: Buy peacock feathers' for row in rows),
+            "New to-do item did not appear in table"
         )
 
         # There is still a text box inviting her to add another item. She
@@ -55,5 +56,3 @@ class NewVisitorTest(unittest.TestCase):
         # methodical)
         self.fail('Finish the test!')
 
-if __name__ == '__main__':  
-    unittest.main(warnings='ignore')  
