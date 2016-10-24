8aa5dca9faae00d3a224249642e8ceae0b4d67cc
neekburm <johnsonscottd@gmail.com>
2016-10-24 08:48:58 -0500

    check the changes to functional_tests.py

--- lists/views.py
+++ lists/views.py
@@ -1,15 +1,9 @@
-<<<<<<< HEAD
-=======
+
 from django.http import HttpResponse
->>>>>>> mixedupstuff
 from django.shortcuts import render
 
 # Create your views here.
 def home_page(request):
-<<<<<<< HEAD
-	return render(request, 'home.html')
-=======
 	return render(request, 'home.html', {
         'new_item_text': request.POST.get('item_text', ''),
     })
->>>>>>> mixedupstuff
