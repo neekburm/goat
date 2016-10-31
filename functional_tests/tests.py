49ee384f921837ff1ef806515cb5377f306c02d6
neekburm <johnsonscottd@gmail.com>
2016-10-31 08:45:24 -0500

    refactor templates to use a base template

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -42,27 +42,22 @@ class NewVisitorTest(LiveServerTestCase):
 
         # She types "Buy peacock feathers" into a text box (Edith's hobby
         # is tying fly-fishing lures)
-        # When she hits enter, the page updates, and now the page lists
-        # "1: Buy peacock feathers" as an item in a to-do list table
 
-        inputbox.send_keys('testing\n')
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertAlmostEqual(
-            inputbox.location['x'] + inputbox.size['width'] / 2,
-            512,
-            delta=5
-        )
 
         inputbox.send_keys('Buy peacock feathers')
+
+        # When she hits enter, the page updates, and now the page lists
+        # "1: Buy peacock feathers" as an item in a to-do list table
+        
         inputbox.send_keys(Keys.ENTER)
         edith_list_url = self.browser.current_url
         self.assertRegex(edith_list_url, '/lists/.+')
-        self.check_for_row_in_list_table('1: Buy peacock feathers')
-        
+        self.check_for_row_in_list_table('1: Buy peacock feathers')        
 
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very
         # methodical)
+
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
@@ -105,14 +100,14 @@ class NewVisitorTest(LiveServerTestCase):
 
         # Satisfied, the both go back to sleep
 
-        # Edith wonders whether the site will remember her list. Then she sees
-        # that the site has generated a unique URL for her -- there is some
-        # explanatory text to that effect.
+    def test_layout_and_styling(self):
+        # Edith goes to the home page
+        self.browser.get(self.live_server_url)
 
-        # She visits that URL - her to-do list is still there.
+        inputbox = self.browser.find_element_by_id('id_new_item')
 
-        # She starts a new list and sees the input is nicely
-        # centered there too
-
-        
-
+        self.assertAlmostEqual(
+        inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=5
+        )
